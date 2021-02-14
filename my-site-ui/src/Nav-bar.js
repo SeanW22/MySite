b734e444aa32df462f4dc451710bfc57e798405c
Sean <seansimonwright@gmail.com>
2021-02-14 18:16:27 +0000

    added Nav bar

---
+++ my-site-ui/src/Nav-bar.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import React, { Component } from 'react';
+
+class NavBar extends Component {
+  render() {
+    return (
+      <header className="App-header">
+        <h1>Hello</h1>
+      </header>
+    );
+  }
+}
+
+export default NavBar;
